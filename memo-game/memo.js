347c0286280264d9de6d6a45ede20ecead341b66
Jeremy <lavigne.jeremy@post.com>
2020-03-26 17:37:19 +0100

    memo game is working fine, nothing to add right now

--- memo-game/memo.js
+++ memo-game/memo.js
@@ -10,6 +10,7 @@
 /* Global variables */
 
 var level = 1;
+var bestLevel = 1;
 var expectedLetters = []; // Will contain random letters 
 var userLetters = []; // Will contain user input
 var timeToMemorize = 4800; // Time to display 3 letters on level 1
@@ -22,6 +23,7 @@ document.getElementById("ready-memorize-btn").addEventListener('click', function
 
     waitForIt();
     concentrationMsg();
+    addBackground();
     
     generateLetters(level);
     showLetters();
@@ -30,12 +32,27 @@ document.getElementById("ready-memorize-btn").addEventListener('click', function
         
         goForIt();
         continueMsg();
+        prepareInput();
+        removeBackground();
         
         document.body.addEventListener('keypress', waitForUserInput);
         
     }, timeToMemorize);
 });
 
+/* ------- New Game ------ */
+
+document.getElementById("new-game-btn").addEventListener('click', function(){
+
+    cleanArrays();
+    cleanEventKeyboard();
+    cleanMessages();
+    
+    welcomeMsg();
+    
+    level = 1;
+    updateInfos();
+});
 
 /* ========== functions =============== */
 
@@ -55,53 +72,83 @@ function generateLetters(level) {
             aleaBox = Math.floor(Math.random()*18) + 1;
         }
         emptyBox[aleaBox] = false;
+        
         expectedLetters.push([alphabet[aleaLetter], aleaBox]);
     }
-    console.log(expectedLetters);
+   /* console.log(expectedLetters); // For development  */
 }
 
 function showLetters() {
+    
     for (var i = 0; i < expectedLetters.length ; i++) {
+        
         var content = expectedLetters[i][0];
         var id = "box" + expectedLetters[i][1];
-        var time = i * 800;
+        
+        var time = i * 800; // Display letter every 800ms 
+        
         showThisLetter(id, content, time);
     }
 } 
 
 function showThisLetter(id, content, time) {
+    
     setTimeout(function(){
         document.getElementById(id).innerHTML = content;
     }, time);
+    
     setTimeout(function(){
         document.getElementById(id).innerHTML = "";
-    }, time + 2400);
+    }, time + 2400); // Letter stays 2400ms
 }
 
-/* -------------- */
+
+/* ------ user keyboard input -------- */
+
+function waitForUserInput(event) {
+            
+    var userLetter = String.fromCharCode(event.charCode);
+    userLetters.push(userLetter);
+    event.stopImmediatePropagation();
+    
+    showInput(userLetter);
+            
+    ifDefeat();
+    ifSuccess();
+    ifUltimateWin();
+}
 
 
 /* ------ Success / Defeat -------- */
 
 function ifSuccess() {
+    
     if(successLetter()) {
         goodLetterMsg();
     }
+    
     if(successAll()) {
         successMsg();
+        
+        timeToMemorize += 800;
         level += 1;
+        improvedRecord();
         updateInfos();
+        
         cleanMessages();
         cleanArrays();
-        cleanEvent();
+        cleanEventKeyboard();
     }
+    
     else {
         return;
     }
 }
 
 function successLetter() {
+    
     var i = userLetters.length - 1;
+    
     if (userLetters[i] === expectedLetters[i][0]) {
         return true;
     } else {
@@ -110,17 +157,24 @@ function successLetter() {
 }
 
 function successAll() {
+    
     for (var i = 0; i < expectedLetters.length ; i++) {
+        
         if (userLetters[i] !== expectedLetters[i][0]) {
             return false;
         }
+        
     }
     return true;
 }
 
 function ifDefeat() {
+    
     if(defeat()) {
         defeatMsg();
+        showAnswer();
+        cleanEventKeyboard();
+        removeGoForIt();
     }
     else {
         return;
@@ -128,7 +182,9 @@ function ifDefeat() {
 }
 
 function defeat() {
+    
     var i = userLetters.length - 1;
+    
     if (userLetters[i] !== expectedLetters[i][0]) {
         return true;
     } else {
@@ -136,94 +192,225 @@ function defeat() {
     }    
 }
 
+function ifUltimateWin() {
+    if(successAll() && (level === 15)) {
+        ultimateWinMsg();
+        
+        level += 1;
+        improvedRecord();
+        updateInfos();
+        
+        cleanMessages();
+        cleanArrays();
+        cleanEventKeyboard();
+        
+        incredible();
+    }
+    
+    else {
+        return;
+    }   
+}
+
 
 /* ------ Informations -------- */
 
 function waitForIt() {
+    
     document.getElementById("ready-memorize-btn").style.display = "none";
     document.getElementById("wait-for-it").style.display = "block"; 
+    
 }
 
 function goForIt() {
+    
     document.getElementById("wait-for-it").style.display = "none";
     document.getElementById("go-for-it").style.display = "block"; 
+    
+}
+
+function removeGoForIt() {
+    
+    document.getElementById("go-for-it").style.display = "none"; 
+    document.getElementById("just-type").style.display = "none"; 
+    
+}
+
+function incredible() {
+    
+    document.getElementById("ready-to-memorize").style.display = "none";
+    document.getElementById("incredible").style.display = "block"; 
+    
 }
 
 function readyToMemorizeBtn() {
+    
     document.getElementById("go-for-it").style.display = "none";
     document.getElementById("user-input").style.display = "none";
     document.getElementById("ready-memorize-btn").style.display = "block"; 
+    
+}
+
+function prepareInput() {
+    
+    document.getElementById("user-input").style.display = "block";
 }
 
 function showInput(userLetter) {
-    document.getElementById("user-input").style.display = "block";
+    
     document.getElementById("user-input-letter").textContent += userLetter + " - ";
+    
+}
+
+function showAnswer() {
+    
+    answerMsg();
+    
+    var end = expectedLetters.length - 1;
+    
+    for (var i = 0 ; i < (end) ; i ++) {
+        document.getElementById("answer-msg-letters").textContent += expectedLetters[i][0] + " - ";
+    }
+    
+    document.getElementById("answer-msg-letters").textContent += expectedLetters[end][0];
+    
 }
 
 
 /* ------ Level / Record -------- */
 
 function updateInfos() {
+    
     document.getElementById("level-number").innerHTML = level;
-    document.getElementById("best-level-number").innerHTML = level; 
+    document.getElementById("best-level-number").innerHTML = bestLevel; 
+    
+}
+
+function improvedRecord() {
+    if (level > bestLevel) {
+        bestLevel = level;
+    }
 }
 
 
-/* ------ Re initialize -------- */
+/* ------ Re-initialize -------- */
 
 function cleanArrays() {
+    
     expectedLetters = [];
     userLetters = [];
+    
 }
 
 function cleanMessages() {
+    
     readyToMemorizeBtn();
+    
     document.getElementById("user-input-letter").innerHTML = "";
+    document.getElementById("answer-msg-letters").innerHTML = "";
+
+    document.getElementById("continue-msg").style.display = "none"; 
 }
 
-function cleanEvent() {
+function cleanEventKeyboard() {
+    
     document.body.removeEventListener('keypress', waitForUserInput);
+    
 }
 
 
 /* ------- Messages ------- */
 
-function concentrationMsg() {
-    document.getElementById("welcome-msg").style.display = "none";
+function welcomeMsg() {
+    
     document.getElementById("success-msg").style.display = "none";
+    document.getElementById("defeat-msg").style.display = "none";
+    document.getElementById("answer-msg").style.display = "none";
+    
+    document.getElementById("welcome-msg").style.display = "block";  
+}
+
+function concentrationMsg() {
+    
+    document.getElementById("success-msg").style.display = "none";
+    document.getElementById("defeat-msg").style.display = "none";
+    document.getElementById("answer-msg").style.display = "none";
+    document.getElementById("welcome-msg").style.display = "none";
+    
     document.getElementById("concentration-msg").style.display = "block";    
+    
 }
 
 function continueMsg() {
+    
     document.getElementById("concentration-msg").style.display = "none";
     document.getElementById("continue-msg").style.display = "block";     
 }
 
 function goodLetterMsg() {
+    
     document.getElementById("good-letter-msg").style.display = "block";
-        setTimeout(function(){
+    
+    setTimeout(function(){
         document.getElementById("good-letter-msg").style.display = "none";
     }, 400);
 }
 
 function successMsg() {
+    
     document.getElementById("continue-msg").style.display = "none";
     document.getElementById("success-msg").style.display = "block";     
 }
 
 function defeatMsg() {
+    
     document.getElementById("continue-msg").style.display = "none";
-    document.getElementById("defeat-msg").style.display = "block";     
+    document.getElementById("defeat-msg").style.display = "block"; 
+    
 }
 
-function waitForUserInput(event) {
-            
-    var userLetter = String.fromCharCode(event.charCode);
-    userLetters.push(userLetter);
-    event.stopImmediatePropagation();
+function answerMsg() {
 
-    showInput(userLetter);
-            
-    ifDefeat();
-    ifSuccess();    
+    document.getElementById("answer-msg").style.display = "block"; 
+    
+}
+
+function ultimateWinMsg() {
+    
+    document.getElementById("continue-msg").style.display = "none";
+    document.getElementById("ultimate-win-msg").style.display = "block";     
+}
+
+/* ------- Visual effects ------- */
+
+function addBackground() {
+    
+    document.getElementById("letters-bloc").style.backgroundColor = "white";
+    
+}
+
+function removeBackground() {
+    
+    document.getElementById("letters-bloc").style.backgroundColor = "rgba(0,0,0,0)";
+    
+}
+
+/* ------- Text info when user look for a place to type ------- */
+
+document.getElementById("user-input").addEventListener("mouseover", justType);
+
+document.getElementById("user-input").addEventListener("mouseout", removeJustType);
+
+function justType() {
+    
+    document.getElementById("go-for-it").style.display = "none"; 
+    document.getElementById("just-type").style.display = "block"; 
+    
+}
+
+function removeJustType(){
+    
+    document.getElementById("just-type").style.display = "none";
+    document.getElementById("go-for-it").style.display = "block";
+    
 }
